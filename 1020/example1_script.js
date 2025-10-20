e2d0d2767eb5d566d527533572670f06aec84690
99YiFei <408630811@o365.tku.edu.tw>
2025-10-20 10:50:50 +0800

    1020

--- 1020/example1_script.js
+++ 1020/example1_script.js
@@ -7,6 +7,12 @@ alert('歡迎來到 JavaScript！');
 // 在 Console 顯示訊息
 console.log('Hello JavaScript from console');
 
+//點擊跳出
+alert('點擊');
+
 // 在頁面指定區域輸出文字
+var el = document.getElementById('result');
+el.textContent = '這行文字是由外部 JS 檔案寫入的。';
+
 var el = document.getElementById('result');
 el.textContent = '這行文字是由外部 JS 檔案寫入的。';
