d36556cadca04c19601fc70cf38c069adb2feb6a
99YiFei <408630811@o365.tku.edu.tw>
2025-10-20 12:28:35 +0800

    1020

---
+++ 1020/example6_script.js
@@ -0,0 +1,54 @@
+// example6_script.js
+// 將使用者輸入的逗號分隔數字字串，轉成陣列並計算總和與平均
+
+var raw = prompt('請輸入多個數字（以逗號,分隔）：');
+var result = '';
+
+if (!raw) {
+    result = '未輸入資料';
+} else {
+    var parts = raw.split(',');
+    var nums = [];
+    for (var i = 0; i < parts.length; i++) {
+        var n = parseFloat(parts[i]);
+        if (!isNaN(n)) {
+            nums.push(n);
+        }
+    }
+
+    if (nums.length === 0) {
+        result = '沒有可用的數字';
+    } else {
+        var sum = 0;
+        for (var j = 0; j < nums.length; j++) {
+            sum += nums[j];
+        }
+        var avg = sum / nums.length;
+        result = '有效數字：' + nums.join(', ') + '\n'
+            + '總和：' + sum + '\n'
+            + '平均：' + avg;
+        //最大值
+        var max = 0;
+        for (var m = 0; nums[m]; m++) {
+            if (nums[m] > max){
+                max = nums[m];
+            }else{
+
+            }
+        }
+        result += '\n最大值為' + max + '\n';
+        //最小值
+        var min = nums[0];
+        for (var n = 1; nums[n]; n++) {
+            if (nums[n] < min){
+                min = nums[n];
+            }else{
+
+            }
+        }
+        result += '最小值為' + min + '\n';
+    }
+}
+
+
+document.getElementById('result').textContent = result;
